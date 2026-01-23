22f4335fad30979e4a6b101708f6813cbc27022d
WILLIAM GONCALVES <williamdouglas2110@gmail.com>
2026-01-23 14:01:27 -0300

    Add Pessoa class with attributes and methods

--- src/model/Pessoa.java
+++ src/model/Pessoa.java
@@ -1,1 +1,50 @@
+package model;
+
+public class Pessoa {
+
+    private String nome;
+    private int idade;
+    private char sexo;
+
+    public Pessoa(String nome, int idade, char sexo) {
+        this.nome = nome;
+        this.idade = idade;
+        this.sexo = sexo;
+    }
+
+    @Override
+    public String toString(){
+        return this.nome;
+    }
+
+    public void fazerAniver(){
+
+        setIdade(getIdade()+1);
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String name) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+}
 
