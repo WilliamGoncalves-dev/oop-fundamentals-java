0b012ac46711abc6cafe2a2644f66589e8f77f79
WILLIAM GONCALVES <williamdouglas2110@gmail.com>
2026-01-23 14:01:09 -0300

    Add Publicacao interface with publication methods

--- src/model/Publicacao.java
+++ src/model/Publicacao.java
@@ -1,1 +1,12 @@
+package model;
+
+public interface Publicacao {
+
+    public void abrir();
+    public void fechar();
+    public void folhear();
+    public void avancarPag();
+    public void voltarPag();
+
+}
 
