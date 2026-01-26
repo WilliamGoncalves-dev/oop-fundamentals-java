9a816b455994a215d955db07d743e6e463e7039c
WILLIAM GONCALVES <williamdouglas2110@gmail.com>
2026-01-26 15:03:57 -0300

    Implement abstract Usuario class with attributes and methods

--- src/inheritance-class12/Usuario/Usuario.java
+++ src/inheritance-class12/Usuario/Usuario.java
@@ -1,1 +1,51 @@
+package Usuario;
+
+public abstract class Usuario {
+
+    private String nome;
+    private int idade;
+    private char sexo;
+
+    public Usuario(String nome, int idade, char sexo) {
+        this.nome = nome;
+        this.idade = idade;
+        this.sexo = sexo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
+    public final void fazerAniv(){
+        this.idade++;
+    }
+
+    public String detalhar() {
+        return "Usuario.Usuario\n-----------------------" +
+                "\n Nome: " + nome + "," +
+                " idade: " + idade +
+                ", sexo: " + sexo;
+    }
+}
+
 
