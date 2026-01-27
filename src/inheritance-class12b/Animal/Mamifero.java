ebfa63e05d94f1133eacb107c3ead5d827271d35
WILLIAM GONCALVES <williamdouglas2110@gmail.com>
2026-01-27 10:04:16 -0300

    feat: adiciona classe Ave estendendo Animal

--- src/inheritance-class12b/Animal/Mamifero.java
+++ src/inheritance-class12b/Animal/Mamifero.java
@@ -1,1 +1,36 @@
+package Animal;
+
+public class Mamifero extends Animal{
+
+
+    public Mamifero(double peso, int idade, int membros, String corPelo) {
+        super(peso, idade, membros);
+        this.corPelo = corPelo;
+    }
+
+    public String getCorPelo() {
+        return corPelo;
+    }
+
+    public void setCorPelo(String corPelo) {
+        this.corPelo = corPelo;
+    }
+
+    private String corPelo;
+
+    @Override
+    public void locomover() {
+
+    }
+
+    @Override
+    public void alimentar() {
+
+    }
+
+    @Override
+    public void emitirSom() {
+
+    }
+}
 
