b90b36c66bdbd07f83387782c8696214cd56a182
WILLIAM GONCALVES <williamdouglas2110@gmail.com>
2026-01-27 10:39:36 -0300

    Implement Peixe class methods for locomotion and feeding

--- src/inheritance-class12b/Animal/Peixe.java
+++ src/inheritance-class12b/Animal/Peixe.java
@@ -24,15 +24,21 @@ public class Peixe extends Animal{
     @Override
     public void locomover() {
 
+        System.out.println("Nadando");
+
     }
 
     @Override
     public void alimentar() {
 
+        System.out.println("Comendo substâncias");
+
     }
 
     @Override
     public void emitirSom() {
+
+        System.out.println("Peixe não faz som");
 
     }
 }
