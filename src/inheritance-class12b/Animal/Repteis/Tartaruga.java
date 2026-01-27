d8dfa20be04c5b7d7d36259817f249033a42904c
WILLIAM GONCALVES <williamdouglas2110@gmail.com>
2026-01-27 13:51:52 -0300

    Add Tartaruga class extending Reptil

---
+++ src/inheritance-class12b/Animal/Repteis/Tartaruga.java
@@ -0,0 +1,16 @@
+package Animal.Repteis;
+
+import Animal.Reptil;
+
+public class Tartaruga extends Reptil {
+
+
+    public Tartaruga(double peso, int idade, int membros, String corEscama) {
+        super(peso, idade, membros, corEscama);
+    }
+
+    @Override
+    public void locomover() {
+        System.out.println("Caminhando a passos lentos");
+    }
+}
