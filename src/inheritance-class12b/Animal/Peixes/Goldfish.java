5a5b5a96eb77bc8991ffde558573ec984da33a6b
WILLIAM GONCALVES <williamdouglas2110@gmail.com>
2026-01-27 13:50:58 -0300

    Add Goldfish.java to Peixes directory

---
+++ src/inheritance-class12b/Animal/Peixes/Goldfish.java
@@ -0,0 +1,11 @@
+package Animal.Peixes;
+
+import Animal.Peixe;
+
+public class Goldfish extends Peixe {
+
+    public Goldfish(double peso, int idade, int membros, String corEscama) {
+        super(peso, idade, membros, corEscama);
+    }
+}
+
