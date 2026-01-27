b043fbfd6e1baa4b5fb80d297256e1fbb159d232
WILLIAM GONCALVES <williamdouglas2110@gmail.com>
2026-01-27 10:38:54 -0300

    Update locomover, alimentar, and emitirSom methods

--- src/inheritance-class12b/Animal/Reptil.java
+++ src/inheritance-class12b/Animal/Reptil.java
@@ -19,16 +19,16 @@ public class Reptil extends Animal{
 
     @Override
     public void locomover() {
-
+        System.out.println("Rastejando");
     }
 
     @Override
     public void alimentar() {
-
+        System.out.println("Comendo vegetais");
     }
 
     @Override
     public void emitirSom() {
-
+        System.out.println("Som de réptil");
     }
 }
