231d7d1a8667676de4d3f1b6145f1aee289aa4e8
WILLIAM GONCALVES <williamdouglas2110@gmail.com>
2026-01-27 10:39:56 -0300

    Refactor Ave class methods and add functionality

--- src/inheritance-class12b/Animal/Ave.java
+++ src/inheritance-class12b/Animal/Ave.java
@@ -1,30 +1,36 @@
 package Animal;
 
 public class Ave extends Animal{
-    
+
     protected String corPena;
 
     public Ave(double peso, int idade, int membros, String corPena) {
         super(peso, idade, membros);
         this.corPena = corPena;
     }
-    
+
     public void fazerNinho(){
-        
+
     }
 
     @Override
     public void locomover() {
-        
+
+        System.out.println("Voando");
+
     }
 
     @Override
     public void alimentar() {
 
+        System.out.println("Comendo frutas");
+
     }
 
     @Override
     public void emitirSom() {
+
+        System.out.println("Som de ave");
 
     }
 }
