a513f7e0f7fdcf577bae7161c5df275a342f63ca
WILLIAM GONCALVES <williamdouglas2110@gmail.com>
2026-01-27 13:49:18 -0300

    Add Arara class extending Ave

---
+++ src/inheritance-class12b/Animal/Aves/Arara.java
@@ -0,0 +1,10 @@
+package Animal.Aves;
+
+import Animal.Ave;
+
+public class Arara extends Ave {
+
+    public Arara(double peso, int idade, int membros, String corPena) {
+        super(peso, idade, membros, corPena);
+    }
+}
