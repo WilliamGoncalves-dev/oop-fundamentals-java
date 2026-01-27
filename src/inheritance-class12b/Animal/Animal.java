b0ce4c37b0e9e207153a00886f3287ff61464d4a
WILLIAM GONCALVES <williamdouglas2110@gmail.com>
2026-01-27 09:44:40 -0300

    feat: Definição dos métodos e atributos da classe

---
+++ src/inheritance-class12b/Animal/Animal.java
@@ -0,0 +1,13 @@
+package Animal;
+
+public abstract class Animal {
+
+    private double peso;
+    private int idade;
+    private int membros;
+
+    public abstract void locomover();
+    public abstract void alimentar();
+    public abstract void emitirSom();
+
+}
